96eae86cc5530081b5cbaebd1baeb3f9089be355
jlu1211 <jlu1211@uw.edu>
2024-10-02 00:14:50 -0700

    Oct 2

--- HW/HW 1/HW1.py
+++ HW/HW 1/HW1.py
@@ -38,12 +38,11 @@ def bisection(left, right, tol=1e-6):
 # %%
 newton_x_final_val, newton_iter_num, newton_x_val = newton_raphson(-1.6)
 A1 = newton_x_val
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A1.npy', newton_x_val)
+
 bisec_x_final_val, bisec_iter_num, bisec_x_val = bisection(-0.7, -0.4)
 A2 = bisec_x_val
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A2.npy', bisec_x_val)
+
 A3 = [newton_iter_num, bisec_iter_num]
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A3.npy', [newton_iter_num, bisec_iter_num])
 
 # %%
 # Question 2
@@ -57,38 +56,27 @@ z = np.array([[1], [2], [-1]])
 
 # a)
 A4 = A + B
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A4.npy', A4)
 
 # b)
 A5 = 3*x - 4*y
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A5.npy', A5)
 
 # c)
 A6 = np.dot(A, x)
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A6.npy', A6)
 
 # d)
 A7 = np.dot(B, (x-y))
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A7.npy', A7)
 
 # e)
 A8 = np.dot(D, x)
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A8.npy', A8)
 
 # f)
 A9 = np.dot(D, y) + z
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A9.npy', A9)
 
 # g)
 A10 = np.dot(A, B)
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A10.npy', A10)
 
 # h)
 A11 = np.dot(B, C)
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A11.npy', A11)
 
 # i)
 A12 = np.dot(C, D)
-np.save('/Users/chris/Library/CloudStorage/OneDrive-UW/4. Seinor/AMATH 481/HW/HW 1/A12.npy', A12)
-
-
